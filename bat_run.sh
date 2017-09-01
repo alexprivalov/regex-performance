78f28454a9d2366e86bacada63e03be89ecc5fe9
Zhe Fu <fooozhe@gmail.com>
2017-08-31 20:43:26 -0700

    results for hscan-multiple

--- bat_run.sh
+++ bat_run.sh
@@ -8,7 +8,7 @@ do
     NAME=${NAME%.*}
 	echo $NAME
 
-    ~/regex-performance/build/src/regex_perf -n 5 -m 0 -i $RULE_FILE -f ~/regex-performance/3200.txt -o ~/regex-performance/$NAME.xls
+    ~/regex-performance/build/src/regex_perf -n 5 -m 1 -i $RULE_FILE -f ~/regex-performance/3200.txt -o ~/regex-performance/${NAME}-hs-mul.xls
 
 done
 echo 'done'
